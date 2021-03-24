913abf35963a7e21ad9fd1e0b4d822c7698b042a
CristianTayan <cris17j@gmail.com>
2021-03-23 22:35:46 -0500

    Change

---
+++ src/app/home/radio_list/radioList.ts
@@ -0,0 +1,12 @@
+export const tracks = [
+    {
+        name: 'Radio Alegría FM',
+        artist: '91.3',
+        cover: 'assets/back.jpeg',
+        source: 'https://cloudstream2032.conectarhosting.com:7092/stream?type=http&nocache=3',
+        icon: 'suryan.png',
+        url: 'https://cloudstream2032.conectarhosting.com:7092/stream?type=http&nocache=3',
+        favorited: false
+    },
+    
+];
